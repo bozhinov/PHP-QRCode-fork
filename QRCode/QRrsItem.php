de49af787fc6591fa5fa54979fe2a4337d5b4bb8
Momchil Bozhinov <momchil@bojinov.info>
2019-02-10 13:09:13 +0200

    * Cleanup QRrsItem
    Some unsafe changes .. oh well

--- QRCode/QRrsItem.php
+++ QRCode/QRrsItem.php
@@ -17,17 +17,16 @@ namespace QRCode;
 
 class QRrsItem {
 
-	public $mm;                  // Bits per symbol 
-	public $nn;                  // Symbols per block (= (1<<mm)-1) 
-	public $alpha_to = [];  // log lookup table 
-	public $index_of = [];  // Antilog lookup table 
-	public $genpoly = [];   // Generator polynomial 
-	public $nroots;              // Number of generator roots = number of parity symbols 
-	public $fcr;                 // First consecutive root, index form 
-	public $prim;                // Primitive element, index form 
-	public $iprim;               // prim-th root of 1, index form 
-	public $pad;                 // Padding bytes in shortened block 
-	public $gfpoly;
+	private $mm;			// Bits per symbol 
+	private $nn;			// Symbols per block (= (1<<mm)-1) 
+	private $alpha_to = [];	// log lookup table 
+	private $index_of = [];	// Antilog lookup table 
+	private $genpoly = [];	// Generator polynomial 
+	private $nroots;		// Number of generator roots = number of parity symbols 
+	#private $fcr;			// First consecutive root, index form 
+	#private $prim;			// Primitive element, index form 
+	#private $iprim;		// prim-th root of 1, index form 
+	private $pad;			// Padding bytes in shortened block 
 
 	public function modnn($x)
 	{
@@ -67,18 +66,14 @@ class QRrsItem {
 		$this->pad = $pad;
 
 		$this->alpha_to = array_fill(0, $this->nn+1, 0);
-		$this->index_of = array_fill(0, $this->nn+1, 0);
-
-		// PHP style macro replacement ;)
-		$NN =& $this->nn;
-		$A0 =& $NN;
+		$this->index_of = $this->alpha_to;
 
 		// Generate Galois field lookup tables
-		$this->index_of[0] = $A0; // log(zero) = -inf
-		$this->alpha_to[$A0] = 0; // alpha**-inf = 0
+		$this->index_of[0] = $this->nn; // log(zero) = -inf
+		$this->alpha_to[$this->nn] = 0; // alpha**-inf = 0
 		$sr = 1;
 
-		for($i=0; $i<$this->nn; $i++) {
+		for($i = 0; $i < $this->nn; $i++) {
 			$this->index_of[$sr] = $i;
 			$this->alpha_to[$i] = $sr;
 			$sr <<= 1;
@@ -94,19 +89,13 @@ class QRrsItem {
 
 		/* Form RS code generator polynomial from its roots */
 		$this->genpoly = array_fill(0, $nroots+1, 0);
-
-		$this->fcr = $fcr;
-		$this->prim = $prim;
 		$this->nroots = $nroots;
-		$this->gfpoly = $gfpoly;
-
-		/* Find prim-th root of 1, used in decoding */
-		for($iprim=1;($iprim % $prim) != 0;$iprim += $this->nn); // intentional empty-body loop!
-
-		$this->iprim = (int)($iprim / $prim);
 		$this->genpoly[0] = 1;
+		
+		$root = $fcr * $prim;
 
-		for ($i = 0,$root=$fcr*$prim; $i < $nroots; $i++, $root += $prim) {
+		for ($i = 0; $i < $nroots; $i++) {
+			
 			$this->genpoly[$i+1] = 1;
 
 			// Multiply rs->genpoly[] by  @**(root + x)
@@ -119,8 +108,10 @@ class QRrsItem {
 			}
 			// rs->genpoly[0] can never be zero
 			$this->genpoly[0] = $this->alpha_to[$this->modnn($this->index_of[$this->genpoly[0]] + $root)];
+
+			$root += $prim;	
 		}
-		
+
 		// convert rs->genpoly[] to index form for quicker encoding
 		for ($i = 0; $i <= $nroots; $i++){
 			$this->genpoly[$i] = $this->index_of[$this->genpoly[$i]];
@@ -129,28 +120,21 @@ class QRrsItem {
 
 	public function encode_rs_char($data, $parity)
 	{
-		$A0 = $this->nn;
-
 		$parity = array_fill(0, $this->nroots, 0);
 
-		for($i=0; $i< ($this->nn - $this->nroots-$this->pad); $i++) {
+		for($i = 0; $i < ($this->nn - $this->nroots - $this->pad); $i++) {
 
 			$feedback = $this->index_of[$data[$i] ^ $parity[0]];
-			if($feedback != $A0) {
+			if($feedback != $this->nn) {
 				// feedback term is non-zero
-
-				// This line is unnecessary when GENPOLY[NROOTS] is unity, as it must
-				// always be for the polynomials constructed by init_rs()
-				$feedback = $this->modnn($this->nn - $this->genpoly[$this->nroots] + $feedback);
-
-				for($j=1;$j<$this->nroots;$j++) {
+				for($j=1; $j < $this->nroots; $j++) {
 					$parity[$j] ^= $this->alpha_to[$this->modnn($feedback + $this->genpoly[$this->nroots-$j])];
 				}
 			}
 
 			// Shift 
 			array_shift($parity);
-			if($feedback != $A0) {
+			if($feedback != $this->nn) {
 				array_push($parity, $this->alpha_to[$this->modnn($feedback + $this->genpoly[0])]);
 			} else {
 				array_push($parity, 0);
