9d42c9f5e4e2277c6e85200b2e1c6ffddb544f3c
Momchil Bozhinov <momchil@bojinov.info>
2019-02-11 15:20:54 +0200

    * Added back code comments

--- QRCode/QRBitstream.php
+++ QRCode/QRBitstream.php
@@ -1,4 +1,17 @@
 <?php
+/*
+ * PHP QR Code
+ *
+ * Based on libqrencode C library distributed under LGPL 2.1
+ * Copyright (C) 2006, 2007, 2008, 2009 Kentaro Fukuchi <fukuchi@megaui.net>
+ *
+ * PHP QR Code is distributed under LGPL 3
+ * Copyright (C) 2010 Dominik Dzienia <deltalab at poczta dot fm>
+ *
+ * Code modifications by Momchil Bozhinov <momchil at bojinov dot info>
+ * Last update - 02.2019
+ *
+ */
 
 namespace QRCode;
 
