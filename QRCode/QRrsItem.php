863fb95d46fe03ac4c528a34e694318a8dd1e5d4
Momchil Bozhinov <momchil@bojinov.info>
2019-03-04 16:57:47 +0200

    Update QRrsItem.php

--- QRCode/QRrsItem.php
+++ QRCode/QRrsItem.php
@@ -96,12 +96,7 @@ class QRrsItem {
 
 	private function modnn($x)
 	{
-		while ($x >= 255) {
-			$x -= 255;
-			$x = ($x >> 8) + ($x & 255);
-		}
-
-		return $x;
+		return ($x % 255);
 	}
 
 	private function rsInit(int $nroots, int $pad)
