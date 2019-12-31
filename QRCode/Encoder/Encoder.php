85efd954f507328fc939173e6f6c89348843e11f
Momchil Bozhinov <momchil@bojinov.info>
2019-12-31 13:24:35 +0200

    * globals to the encoder class

--- QRCode/Encoder/Encoder.php
+++ QRCode/Encoder/Encoder.php
@@ -15,6 +15,18 @@
 
 namespace QRCode\Encoder;
 
+// Encoding modes
+define('QR_MODE_NUM', 0);
+define('QR_MODE_AN', 1);
+define('QR_MODE_8', 2);
+define('QR_MODE_KANJI', 3);
+
+// Levels of error correction.
+define('QR_ECLEVEL_L', 0);
+define('QR_ECLEVEL_M', 1);
+define('QR_ECLEVEL_Q', 2);
+define('QR_ECLEVEL_H', 3);
+
 class Encoder {
 
 	private $dataStr;
--- QRCode/QRCode.php
+++ QRCode/QRCode.php
@@ -6,18 +6,6 @@
 
 namespace QRCode;
 
-// Encoding modes
-define('QR_MODE_NUM', 0);
-define('QR_MODE_AN', 1);
-define('QR_MODE_8', 2);
-define('QR_MODE_KANJI', 3);
-
-// Levels of error correction.
-define('QR_ECLEVEL_L', 0);
-define('QR_ECLEVEL_M', 1);
-define('QR_ECLEVEL_Q', 2);
-define('QR_ECLEVEL_H', 3);
-
 use QRCode\Encoder\Encoder;
 
 class QRCode {
